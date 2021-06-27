e25cd48ee4350088ab00e9aff88d4b45d5b73cbc
ioancw <ioan.williams@icloud.com>
2021-06-27 11:25:26 +0100

    First commit of project

---
+++ FileSearch/Program.fs
@@ -0,0 +1,35 @@
+﻿module FileSearch
+
+open System
+
+open Query
+open Common
+open Argu
+
+type Arguments =
+    | Folder of path: string
+    | Find of query: string
+    interface IArgParserTemplate with
+        member t.Usage =
+            match t with
+            | Folder _ -> "Specify folder to search."
+            | Find _ -> "What to search for."
+
+[<EntryPoint>]
+let main args =
+
+    let parser =
+        ArgumentParser.Create<Arguments>(programName = "FileSearch")
+
+    let parsedArgs = parser.Parse()
+    let folder = parsedArgs.GetResult Folder
+    let find = parsedArgs.GetResult Find
+
+    let currentColour = Console.ForegroundColor
+
+    do apply Colours.gray
+
+    do searchFor folder find
+
+    do apply currentColour
+    0 // return an integer exit code
---
+++ FileSearch/search.fs
@@ -0,0 +1,71 @@
+﻿module Search
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+open Common
+
+//found list of files to 'examine' further in order to find the line numbers where the search words exist.
+type SearchResult(lineNo: int, file: string, content: string, tokens: Token []) =
+    member t.LineNo = lineNo
+    member t.File = file
+    member t.Content = content
+    member t.Tokens = tokens
+
+let read (file: string) =
+    seq {
+        use reader = new StreamReader(file)
+
+        while not reader.EndOfStream do
+            yield reader.ReadLine()
+    }
+
+let searchFile tokens parse check file =
+    read file
+    |> Seq.mapi (fun i l -> i + 1, parse l)
+    |> Seq.filter check
+    |> Seq.map (fun (i, l) -> SearchResult(i, file, l, tokens))
+
+let search tokens parse check =
+    Seq.map (fun file -> searchFile tokens parse check file)
+    >> Seq.concat
+
+let printLineWithHighlight line tokens =
+    //In order to do a Regex.Split (and return the word you split on) you need
+    //to form the regex string as "(Dave)|(David)""
+    let regex =
+        tokens
+        |> Array.map (
+            (fun (Token t) -> t)
+            >> (fun t -> sprintf "(%s)" t)
+        )
+        |> stringJoinS "|"
+
+    Regex.Split(line, regex)
+    |> Array.iter
+        (fun s ->
+            if Array.contains (Token s) tokens then
+                printc Colours.magenta s
+            else
+                printc Colours.darkgreen s)
+
+    printc Colours.darkyellow (sprintf "\n")
+
+let printResults : seq<string * seq<SearchResult>> -> unit =
+    do apply Colours.yellow
+    //group search results
+    Seq.fold
+        (fun c (path, results) ->
+            printc Colours.darkyellow (sprintf "%A\n" path)
+            let size = results |> Seq.length
+
+            results
+            |> Seq.iter
+                (fun sr ->
+                    printc Colours.gray (sprintf "line %d " sr.LineNo)
+                    printLineWithHighlight sr.Content sr.Tokens)
+
+            c + size)
+        0
+    >> printfn "%d results"
